d92b73ad9689a5e464be35557c561c20da2e9a8a
webboy <extramedia.nemanja@gmail.com>
2025-02-18 09:21:21 +0100

    Fix env

--- app/services/twilio_service.py
+++ app/services/twilio_service.py
@@ -4,12 +4,12 @@ import os
 
 class TwilioService:
     def __init__(self):
-        self.account_sid = os.environ['TWILIO_ACCOUNT_SID']
-        self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
-        self.sender_number = os.environ['TWILIO_PHONE_NUMBER']
+        self.account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+        self.auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        self.sender_number = os.getenv('TWILIO_PHONE_NUMBER')
         self.client = Client(self.account_sid, self.auth_token)
 
-    def send(self, to, message):
-        message = self.client.messages.create(to=to, from_=self.sender_number, body=message)
+    def send(self, to, message_text):
+        message = self.client.messages.create(to=to, from_=self.sender_number, body=message_text)
 
         return message
