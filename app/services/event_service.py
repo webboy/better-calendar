10f0abe1768db05f98349745380d37ad63f4bddd
Joud Hajal <joudhajal@Mac.fritz.box>
2025-02-18 16:36:31 +0100

    Finished add_events for event_service.py

--- app/services/event_service.py
+++ app/services/event_service.py
@@ -23,11 +23,40 @@ class EventService:
             events_container = []
             for event in self.events.keys():
 
-                events_container.append(self.events[event]['Description'])
+                events_container.append(self.events[event]['description'])
 
             return events_container
         else:
             raise "You have no upcoming events"
+
+
+    def add_events(self, host, location,  event_name, event_date, event_time, guests):
+
+        try:
+            date_time_str = f'{event_date} {event_time}'
+
+            valid_date_time = datetime.strptime(date_time_str, '%d-%m-%Y %H:%M:%S')
+            _date = datetime.strptime(event_date, "%d-%m-%Y")
+            _time = datetime.strptime(event_time, "%H:%M:%S")
+            _description = f"{event_name} on {event_date} at {event_time}"
+            _event  = {}
+        except ValueError:
+            raise "Invalid date and time format"
+
+        self.events[event_name] = {'name': event_name,
+                                   'host': host,
+                                   'location': location,
+                                   'date': event_date,
+                                   'time': event_time,
+                                   "day_name": datetime.strftime(_date, '%A'),
+                                   'is_weekday': valid_date_time.weekday() < 5 ,
+                                   'month_name': datetime.strftime(_date, '%B'),
+                                   'guests': guests,
+                                   'description': _description}
+
+        with open('events.json', 'w') as file:
+            json.dump(self.events, file, indent=4)
+
 
 
 
@@ -39,4 +68,5 @@ class EventService:
 
 es = EventService()
 
-print(es.list_events())
+#print(es.list_events())
+es.add_events('nem', 'zoom', "added_event", "10-10-2010", "10:10:10", ['Joud', "kehalit"])
