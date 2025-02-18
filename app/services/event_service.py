258ce92fbc7dd8cda1b2b397c18b4f7dc86c4794
Joud Hajal <joudhajal@Mac.fritz.box>
2025-02-18 15:50:45 +0100

    Added list_events for event_service

--- app/services/event_service.py
+++ app/services/event_service.py
@@ -1,7 +1,42 @@
 from typing import List
+import json
+from datetime import datetime
 
 
 class EventService:
+
+    def __init__(self):
+
+
+
+        with open('events.json', 'r') as json_file:
+            self.events = json.load(json_file)
+
+
+
+
     def list_events(self) -> List[str]:
+
         """Placeholder for list_events - to be implemented"""
-        return ["Event1", "Event2"]
+        if self.events:
+
+            events_container = []
+            for event in self.events.keys():
+
+                events_container.append(self.events[event]['Description'])
+
+            return events_container
+        else:
+            raise "You have no upcoming events"
+
+
+
+
+
+
+
+
+
+es = EventService()
+
+print(es.list_events())
