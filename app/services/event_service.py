0cb5ece88699753930c78a54603f8682e682d719
Joud Hajal <joudhajal@Mac.fritz.box>
2025-02-18 17:09:34 +0100

    Added update_event and created update_date_related_fields

--- app/services/event_service.py
+++ app/services/event_service.py
@@ -1,89 +1,90 @@
-from typing import List
+from typing import List, Dict, Any
 import json
 from datetime import datetime
 
 
 class EventService:
 
-    def __init__(self):
+    def __init__(self, file_path: str = 'events.json'):
+        self.file_path = file_path
+        self.events = self.load_events()
 
-
-
-        with open('events.json', 'r') as json_file:
-            self.events = json.load(json_file)
-
-
-
+    def load_events(self) -> Dict[str, Any]:
+        try:
+            with open(self.file_path, 'r') as json_file:
+                return json.load(json_file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return {}
 
     def list_events(self) -> List[str]:
-
-        """Placeholder for list_events - to be implemented"""
+        """Returns a list of event descriptions."""
         if self.events:
-
-            events_container = []
-            for event in self.events.keys():
-
-                events_container.append(self.events[event]['description'])
-
-            return events_container
+            return [event_data['description'] for event_data in self.events.values()]
         else:
-            raise "You have no upcoming events"
+            raise ValueError("You have no upcoming events")
 
-
-    def add_events(self, host, location,  event_name, event_date, event_time, guests):
-
-        try:
-            date_time_str = f'{event_date} {event_time}'
-
-            valid_date_time = datetime.strptime(date_time_str, '%d-%m-%Y %H:%M:%S')
-            _date = datetime.strptime(event_date, "%d-%m-%Y")
-            _time = datetime.strptime(event_time, "%H:%M:%S")
-            _description = f"{event_name} on {event_date} at {event_time}"
-            _event  = {}
-        except ValueError:
-            raise "Invalid date and time format"
-
-        self.events[event_name] = {'name': event_name,
-                                   'host': host,
-                                   'location': location,
-                                   'date': event_date,
-                                   'time': event_time,
-                                   "day_name": datetime.strftime(_date, '%A'),
-                                   'is_weekday': valid_date_time.weekday() < 5 ,
-                                   'month_name': datetime.strftime(_date, '%B'),
-                                   'guests': guests,
-                                   'description': _description}
-
-        with open('events.json', 'w') as file:
+    def update_db(self) -> None:
+        """Updates the JSON file with the current events."""
+        with open(self.file_path, 'w') as file:
             json.dump(self.events, file, indent=4)
 
-
-
-    def remove_event(self, event_name):
-
+    def add_events(self, host: str, location: str, event_name: str, event_date: str, event_time: str, guests: List[str]) -> None:
+        """Adds a new event to the events list."""
         try:
+            valid_date_time = datetime.strptime(f'{event_date} {event_time}', '%d-%m-%Y %H:%M:%S')
+        except ValueError:
+            raise ValueError("Invalid date and time format")
+
+        event_data = {
+            'name': event_name,
+            'host': host,
+            'location': location,
+            'date': event_date,
+            'time': event_time,
+            'day_name': valid_date_time.strftime('%A'),
+            'is_weekday': valid_date_time.weekday() < 5,
+            'month_name': valid_date_time.strftime('%B'),
+            'guests': guests,
+            'description': f"{event_name} on {event_date} at {event_time}"
+        }
+
+        self.events[event_name] = event_data
+        self.update_db()
+
+    def remove_event(self, event_name: str) -> None:
+        """Removes an event from the events list."""
+        if event_name in self.events:
             del self.events[event_name]
-            with open('events.json', 'w') as file:
-                json.dump(self.events, file, indent=4)
-        except KeyError:
-            raise "Event not found"
+            self.update_db()
+        else:
+            raise KeyError("Event not found")
+
+    def update_event(self, name: str, **kwargs: Any) -> None:
+        """Updates an existing event with the given parameters."""
+        if name not in self.events:
+            raise KeyError("Event not found")
+
+        for key, value in kwargs.items():
+            if key in self.events[name]:
+                self.events[name][key] = value
+                if key == 'date':
+                    self.update_date_related_fields(name, value)
+            else:
+                raise ValueError("Invalid Event Param")
+
+        self.update_db()
+
+    def update_date_related_fields(self, name: str, date: str) -> None:
+        """Updates the date-related fields for an event."""
+        valid_date = datetime.strptime(date, '%d-%m-%Y')
+        self.events[name]['day_name'] = valid_date.strftime('%A')
+        self.events[name]['is_weekday'] = valid_date.weekday() < 5
+        self.events[name]['month_name'] = valid_date.strftime('%B')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+#Small tests
 es = EventService()
-
-#print(es.list_events())
-es.add_events('nem', 'zoom', "add_event", "10-10-2010", "10:10:10", ['Joud', "kehalit"])
-
-
+# print(es.list_events())
+es.add_events('nem', 'zoom', "Joud and kehalit", "10-10-2010", "10:10:10", ['Joud', "kehalit"])
+es.update_event("Joud and kehalit", date="11-11-2011", time="11:11:11")
