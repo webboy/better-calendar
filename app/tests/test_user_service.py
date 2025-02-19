d008692e19c45431ee373fa8595a5375a6e9e705
Joud Hajal <joudhajal1997@hotmail.com>
2025-02-20 00:32:35 +0100

    added wa_id test

--- app/tests/test_user_service.py
+++ app/tests/test_user_service.py
@@ -13,15 +13,16 @@ def test_get_user_by_email_invalid(user_service):
 
 def test_get_user_by_email_valid(user_service):
     user = user_service.get_user_by_email('extramedia.nemanja@gmail.com')
-    assert isinstance(user, User), "Expected User instance but got something else."
+    assert isinstance(user, User), "Fail"
 
 def test_get_user_by_wa_id_valid(user_service):
     user = user_service.get_user_by_wa_id('491724184069')
-    assert isinstance(user, User), "Expected User instance but got something else."
+    assert isinstance(user, User), "Fail"
 
 
 def test_get_user_by_wa_id_invalid(user_service):
-    assert user_service.get_user_by_wa_id('00') is None, "Expected None for non-existent user."
+    assert user_service.get_user_by_wa_id('00') is None, "Fail"
+    assert user_service.get_user_by_wa_id(12) is None, "Fail"
 
 if __name__ == '__main__':
     pytest.main()
